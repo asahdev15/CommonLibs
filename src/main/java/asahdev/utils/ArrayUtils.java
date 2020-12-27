65f8944b150945f07ea93fc8b8d54456e505cf9d
Ashish Sahdev <ashishsahdev15@yahoo.in>
2020-12-27 11:02:35 +0530

    Updates

--- src/main/java/asahdev/utils/ArrayUtils.java
+++ src/main/java/asahdev/utils/ArrayUtils.java
@@ -3,7 +3,14 @@ package asahdev.utils;
 import java.util.Arrays;
 
 public class ArrayUtils {
+
+    public static void main(String[] args) {
+        printArrays(new int[]{1,2});
+    }
+
+
     public static void printArrays(int[] array){
         System.out.println(Arrays.toString(array));
     }
+
 }
